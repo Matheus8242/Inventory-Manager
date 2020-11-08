d5979d153de5440a9c593a1de2d76c53e9d5cf22
Matheus1735 <c_matheus@live.com>
2020-11-08 10:02:12 -0500

    Inventory Manager

---
+++ InventoryManager.WinForms/Controls/EquippedItemControl.cs
@@ -0,0 +1,71 @@
+﻿using InventoryManager.Data;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace InventoryManager.WinForms.Controls
+{
+    public partial class EquippedItemControl : UserControl
+    {
+        public Player Player
+        {
+            get => mPlayer;
+            set
+            {
+                if (mPlayer != value)
+                {
+                    mPlayer = value;
+                    if(mPlayer != null)
+                    {
+                        var inventory = new List<Item>(mPlayer.Inventory);
+                        inventory.Insert(0, NoItem);
+
+                        equippedItemComboBox.SelectedIndexChanged -= EquippedItemComboBox_SelectedIndexChanged;
+                        equippedItemComboBox.DataSource = inventory;
+                        EquippedItem = mPlayer.EquippedItems.TryGetValue(EquipLocation, out Item equippedItem) ? equippedItem : NoItem;
+                        equippedItemComboBox.SelectedIndexChanged += EquippedItemComboBox_SelectedIndexChanged;
+                    }
+                    else
+                    {
+                        equippedItemComboBox.DataSource = null;
+                    }
+                }
+            }
+        }
+        public EquipLocations EquipLocation 
+        { 
+            get => mEquipLocation;
+            set
+            {
+                mEquipLocation = value;
+                equipLocationTextBox.Text = mEquipLocation.ToString();
+            }
+        }
+
+        public Item EquippedItem 
+        { 
+            get => (Item)equippedItemComboBox.SelectedItem;
+            set => equippedItemComboBox.SelectedItem = value; 
+        }
+        public EquippedItemControl()
+        {
+            InitializeComponent();
+        }
+        private void EquippedItemComboBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            Item equippedItem = EquippedItem;
+            if(equippedItem == NoItem)
+            {
+                mPlayer.EquippedItems.Remove(EquipLocation);
+            }
+            else
+            {
+                mPlayer.EquippedItems[EquipLocation] = equippedItem;
+            }
+        }
+
+        private static readonly Item NoItem = new Item() { Name = "None"};
+        
+        private Player mPlayer;
+        private EquipLocations mEquipLocation;
+    }
+}
